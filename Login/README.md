0bba7248b222c95541d32da990a1866aa20e1456
kossam <kossamkafesu@gmail.com>
2021-03-22 12:56:36 +0200

    Add files via upload

---
+++ Login/README.md
@@ -0,0 +1,19 @@
+# Login and Registration System
+
+Login System for a group of users. Includes the following functionalities:
+
+* Login homepage
+    * A new user can Register and be able login into the system
+* Dashboard
+    * After logging in the user can be able to 1. view currently registered users
+                                               2. register/add a new user
+                                               3. update/edit and delete users
+                                               4. search for a certain user
+                                               3. Exit and get back to the home page
+
+
+*Installation instructions
+1.Copy the Login folder and paste it in this directory xampp/htdocs
+1.We have a database by the name registration and a table for users. 
+2.Simply create a database in xamp and name it registration and import the (registration.sql) database file.
+3.Type localhost/Login/login.html in your web browser and you are good to go.
---
+++ Login/registration.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: March 21, 2021 at 05:00 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `registration`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(3) NOT NULL DEFAULT '0',
+  `username` varchar(15) DEFAULT NULL,
+  `password` varchar(15) DEFAULT NULL,
+  `page` int(1) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `firstname` varchar(50) NOT NULL,
+  `middlename` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `birthdate` date NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`firstname`, `middlename`, `lastname`, `birthdate`, `username`, `password`) VALUES
+('kossam', 'Lee', 'Kafesu', '2021-03-21', 'kossam', '123456'),
+('Mark', 'E. ', 'Lopez', '2021-07-26', 'mark11', '2d'),
+('Tomas', 'Light', 'Edison', '2021-07-19', 'tom', 'tom');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
